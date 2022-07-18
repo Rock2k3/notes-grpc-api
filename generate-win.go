350555c0bb46aa0765fd5a0542c5b7ae0dcc5b9e
Vadim <rock2k3@yandex.ru>
2022-07-18 12:29:03 +0300

    fix go:generate

--- generate-win.go
+++
@@ -1,3 +0,0 @@
-package NotesGrpcApi
-
-//go:generate ./bin/windows/protoc ./proto/*.proto --go_out=. --go-grpc_out=.
---
+++ generate.go
@@ -0,0 +1,3 @@
+package notes_grpc_api
+
+//go:generate protoc ./proto/*.proto --go_out=. --go-grpc_out=.
